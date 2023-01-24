8af46c916e49013d5129751e78f96f8159975de2
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-01-23 20:20:40 -0700

    removing button to make way for sliders

--- CSE335Lab1Part2/ContentView.swift
+++ CSE335Lab1Part2/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  CSE335Lab1Part2
+//  CSE335Lab1Part1
 //
 //  Created by Arjun Dadhwal on 1/23/23.
 //
@@ -8,9 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var height: String = "";
+    @State var weight: String = "";
+    private var BMI: Float { (Float(weight) ?? 0) / ( (Float(height) ?? 0) * (Float(height) ?? 0) ) * 703 }
+    @State var BMIString: String = "";
+    @State var status: Int = -1;
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            Text("BMI Calculator").foregroundColor(.blue);
+
+        }
+        VStack{
+        
+            VStack{
+        HStack{
+            Text("Height: ")
+            Spacer()
+            Spacer()
+            TextField("", text: $height).keyboardType(.decimalPad);
+            
+        }
+        HStack{
+            Text("Weight: ")
+            Spacer()
+            Spacer()
+            TextField("", text: $weight).keyboardType(.decimalPad);
+        }
+
+        HStack{
+            Text("BMI: ")
+            Spacer()
+            Spacer()
+            
+            if (BMI > 0)
+            {
+            Text("\(self.BMI)");
+            }
+            else
+            {
+                Text("");
+            }
+            
+                }
+            }
+                
+
+        Spacer()
+        
+        HStack{
+            
+            if (BMI > 0 && BMI < 18)
+            {
+                Text("You are Underweight...").foregroundColor(.blue);
+            }
+            
+            else if (BMI >= 18 && BMI < 25 )
+            {
+                Text("You are Normal...").foregroundColor(.green);
+            }
+            
+            else if (BMI >= 25 && BMI <= 30)
+            {
+                Text("You are Pre-Obese...").foregroundColor(.purple);
+            }
+            else if (BMI > 30)
+            {
+                Text("You are Obese...").foregroundColor(.red);
+            }
+            else
+            {
+                Text("");
+            }
+            
+            
+        }
+        
+        }.padding(100)
     }
 }
 
